63d4f5cdba2a534442cb73d2471fa868312fa756
sachin mathew jose <sachin.jose9@gmail.com>
2017-10-12 00:07:37 -0400

    functionality done

--- classes/homePage.class.php
+++ classes/homePage.class.php
@@ -6,7 +6,7 @@ class homePage extends page
     {
         $this->html .= '<h1>File Upload</h1>';
         $form = '';
-        $form .= '<form action="index.php?" method="post" enctype="multipart/form-data">';
+        $form .= '<form action="index.php" method="post" enctype="multipart/form-data">';
         $form .= 'Select file to upload:';
         $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
         $form .= '<input type="submit" value="Upload CSV" name="submit">';
@@ -18,9 +18,7 @@ class homePage extends page
     {
         $target_dir = "uploads/";
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
         $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
-        // Check if image file is a actual image or fake image
         if(isset($_POST["submit"]))
         {
             if ($fileType != 'csv')
@@ -28,21 +26,17 @@ class homePage extends page
                 header('Location: index.php?page=errorReport&errorMsg=Sorry! no csv file found');
                 exit;
             }
-        }
-        else
-        {
-            //$this->html .= 'fail';
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
+            elseif (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
             {
                 header("Location: index.php?page=table&file=$target_file");
                 exit;
             }
             else
             {
-                header('Location: index.php?page=errorReport&errorMsg=Sorry2.');
+                header('Location: index.php?page=errorReport&errorMsg=Sorry an error occured. The file is not saved');
                 exit;
             }
-         }     
+        }     
     }
 
 }
